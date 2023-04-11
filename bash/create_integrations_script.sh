b330cb6f4da12a36bd35efdeae926d899ac32d8a
Andres F Giraldo Granada <andres57706@gmail.com>
2023-04-11 10:45:06 -0500

    feat: agrega script de tareas repetitivas
    Signed-off-by: Andres F Giraldo Granada <andres57706@gmail.com>

---
+++ bash/create_integrations_script.sh
@@ -0,0 +1,67 @@
+#! /bin/bash
+
+# Usage
+if [ $# -eq 0 ]
+then
+  # Print a usage help message
+  echo "Usage: $0 [PART] [WORKING_DIR] \n"
+  echo "\t [PART] script part number(number). Default value = 1"
+  echo "\t [WORKING_DIR] working directory. Should be a git worktree or git repository"
+  exit 1
+fi
+
+# Parse args
+PT=${1:-1}
+WD=${2:-$(pwd)}
+
+# Creading fresh data
+echo "Fresh Ticket Number: "
+read ticket_number
+
+echo "Ticket public link: "
+read ticket_public_link
+
+# create rapanui mutation
+echo "Create rapanui mutation \n"
+
+echo "Type email: "
+read email
+echo "Type email subject: "
+read subject
+echo "Type tenant name: "
+read tenant
+echo "Type country: "
+read country
+
+# working directory should be a git working tree
+echo "
+Working dir: $WD
+"
+cd $WD
+
+# creating worktree with a new branch
+branch="hotfix/${subject// /-}-$tenant-$PT"
+worktree_location="../worktrees/$ticket_number-${subject// /-}-$PT "
+git worktree add -b $branch $worktree_location main &&\
+cd $worktree_location &&\
+
+# execute create mutation rapanui script
+./scripts/create-mutation ${subject// /_}_$PT -e "$email" -s "$subject parte $PT" -t "$tenant" -c "$country" &&\
+
+# add changes
+git add mutations/ &&\
+
+# edit files
+nvim $(git diff --staged --name-only) &&\
+
+# add changes
+git add mutations/ &&\
+
+# commit changes
+git commit -S -s -v -m "fix: $subject part-$PT $tenant $country
+
+[fresh-$ticket_number]($ticket_public_link)
+" &&\
+
+# push and create branch in remote
+git push origin -u $branch
