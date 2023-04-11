4b2a14a7f2d8b0e079c7a69c1a9024ab4a3df296
Andres F Giraldo Granada <andres57706@gmail.com>
2023-04-11 10:24:59 -0500

    feat: soporta recibir items vacio en 2do argumento
    Signed-off-by: Andres F Giraldo Granada <andres57706@gmail.com>

--- ruby/settlements_lines_xlsx_helper.rb
+++ ruby/settlements_lines_xlsx_helper.rb
@@ -1,7 +1,6 @@
 require 'rubyXL'
 require 'pry'
 
-# rubocop:disable Buk/FileNameClass
 # Implementation of String class adds
 # methods to be used by this script
 class String
@@ -13,6 +12,7 @@ class String
     otros_descuentos: 'total_otros_descuentos',
     descuentos_legales: 'total_descuentos_legales',
     total_neto: 'liquido',
+    neto_a_pagar: 'liquido'
   }.freeze
 
   # Transforms a string aplying some rules for special characters
@@ -21,9 +21,8 @@ class String
   # @return [String] A transformed text
   def special_characters
     tr('áéíóú', 'aeiou') # accent rule for vocals
-    .tr(' ', '_') # whitespace transformation
+      .tr(' ', '_') # whitespace transformation
   end
-
 
   # Maps a key from input to a desired value using
   # the MAPPING_KEYS constant Hash
@@ -34,6 +33,7 @@ class String
     MAPPING_KEYS[to_sym] || self
   end
 end
+
 #
 # Own implementation of Hash class
 # with transformation types method
@@ -41,9 +41,9 @@ class Hash
   # Holds the transformation for
   # the desired result keys
   KEY_VALUE_TYPE_TRANSFORMER = {
-    rut: proc {|value| value.to_s},
-    code: proc{|value| value.to_s},
-    monto: proc{|value| value.to_i},
+    rut: proc { |value| value.to_s },
+    code: proc { |value| value.to_s },
+    monto: proc { |value| value.to_i }
   }.freeze
 
   # Transform a hash using the KEY_VALUE_TYPE_TRANSFORMER constant
@@ -57,12 +57,11 @@ class Hash
       elsif v.is_a? Hash
         v.transform_values_types
       elsif v.is_a? Array
-        v.select{|item| item.is_a? Hash}.each(&:transform_values_types)
+        v.select { |item| item.is_a? Hash }.each(&:transform_values_types)
       end
     end
   end
 end
-# rubocop:enable Buk/FileNameClass
 
 # Imports an xlsx file with a single sheet and returns an array of hashes.
 # The first row of the sheet is considered as headers, and each row is
@@ -75,13 +74,13 @@ end
 def import_xlsx(file_path)
   workbook = RubyXL::Parser.parse(file_path)
   sheet = workbook[0]
-  headers = sheet[0].cells.map{|r| r.value.downcase}
+  headers = sheet[0].cells.map { |r| r.value.downcase }
 
   rows = sheet.drop(1).compact
 
   rows.each_with_object([]) do |row, arr|
-    r = headers.zip(row.cells.map(&:value)).to_h
-    arr << r if r.all?{|_, v| !v.nil?}
+    r = headers.zip(row.cells.filter_map { |rw| rw&.value }).to_h
+    arr << r if r.all? { |_, v| !v.nil? }
   end
 end
 
@@ -92,17 +91,19 @@ end
 # @return [Hash] A transformed hash.
 def transform_hash(hash, keys)
   # set a new hash removing desired key values
-  new_hash = hash.reject{|k, _| keys.include?(k)}
+  new_hash = hash.reject { |k, _| keys.include?(k) }
 
-  # Transform desired keys to an array of hashes [{code: 'key', monto: 'value'}]
-  items = hash.select{|k, _| keys.include?(k)}.map do |(k, v)|
-    {'code': k, 'monto': v}
+  if keys.any?
+    # Transform desired keys to an array of hashes [{code: 'key', monto: 'value'}]
+    items = hash.select { |k, _| keys.include?(k) }.map do |(k, v)|
+      { 'code': k, 'monto': v }
+    end
+    new_hash['items'] = items
   end
-  new_hash['items'] = items
-  new_hash.transform_keys!{|k| k.strip.special_characters.map_keys.to_sym}
+
+  new_hash.transform_keys! { |k| k.strip.special_characters.map_keys.to_sym }
   new_hash.transform_values_types
 end
-
 
 if ARGV.length != 2
   puts "Usage: ruby script.rb <file_path> <item_codes>
@@ -119,10 +120,9 @@ end
 file_path = ARGV[0]
 item_codes = ARGV[1].split(',')
 
-
 begin
   rows = import_xlsx(file_path)
-  result = rows.map{|r| transform_hash(r, item_codes)}
+  result = rows.map { |r| transform_hash(r, item_codes) }
   puts result.inspect
 rescue StandardError => e
   puts "Error: #{e.message}"
